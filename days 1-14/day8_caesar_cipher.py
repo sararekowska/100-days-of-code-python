959bc68f8e3c35ad907fb8081d35ef268ca47068
sararekowska <sara.rekowska@gmail.com>
2022-04-17 22:13:03 +0200

    day 8: caesar cipher improvements

--- days 1-14/day8_caesar_cipher.py
+++ days 1-14/day8_caesar_cipher.py
@@ -18,32 +18,40 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 print(logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
-text = input("Type your message: ").lower()
-shift = int(input("Type the shift number: "))
 
 def caesar(direction, text, shift):
     cipher_text = ""
     if direction == "decode":
         shift *= -1
-    for letter in text:
-        index = alphabet.index(letter)
-        new_index = index + shift
-        cipher_text += alphabet[new_index]
+    for char in text:
+        if char in alphabet:
+            index = alphabet.index(char)
+            new_index = index + shift
+            cipher_text += alphabet[new_index]
+        else:
+            cipher_text += char
     print(f"The {direction}d text is {cipher_text}")
 
-if direction == "encode" or direction == "decode":
-    caesar(direction=direction, text=text, shift=shift)
-else:
-    print("Invalid input")
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
+    text = input("Type your message: \n").lower()
+    shift = int(input("Type the shift number: \n"))
+    shift = shift % 25
+
+    if direction == "encode" or direction == "decode":
+        caesar(direction=direction, text=text, shift=shift)
+    else:
+        print("Invalid input")
+    
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
+    if result == "no":
+        should_continue = False
+        print("Goodbye")
 
 ## ALTERNATIVE WAY WITHOUT THE ALPHABET ARRAY
 # i = ord(letter) - ord('a')
-# i += shift
+# i += shift // i -= shift
 # i %= 26
 # cipher_text += chr(ord('a') + i)
-
-# i = ord(letter) - ord('a')
-# i -= shift
-# i %= 26
-# decoded_text += chr(ord('a') + i)
